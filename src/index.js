5ffbdea72c5915f99f7df5c5421691fa2ab871b3
m-avagyan <martikavagyan1@gmail.com>
2023-01-21 15:38:13 +0400

    wip: react app root setup

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const root = document.getElementById("root");
+const app = createRoot(root);
+
+app.render(<h1>test</h1>);
