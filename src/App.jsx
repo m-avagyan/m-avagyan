5b71e30855d042708fd1c69d9102ac4afb7c440f
m-avagyan <martikavagyan1@gmail.com>
2023-01-21 23:55:08 +0400

    wip: css for webpack

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,32 @@ import RecentActivityImage from "images/recent-activity.svg";
 import LanguagesImage from "images/languages.svg";
 import AchievementsImage from "images/achievements.svg";
 
+import classes from "./App.module.css";
+
 function App() {
   return (
-    <table
-      cellSpacing="0"
-      cellPadding="0"
-      style={{ borderCollapse: "collapse", borderSpacing: "none" }}
-    >
-      <tbody>
-        <tr style={{ border: "none" }}>
-          <td width="400px" style={{ border: "noe", verticalAlign: "top" }}>
-            <HeaderImage />
-            <RepositoriesImage />
-            <ActivityCommunityImage />
-            <RecentActivityImage />
-          </td>
-          <td width="400px" style={{ border: "noe", verticalAlign: "top" }}>
-            <LanguagesImage />
-            <AchievementsImage />
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div className={classes.app}>
+      <table
+        cellSpacing="0"
+        cellPadding="0"
+        style={{ borderCollapse: "collapse", borderSpacing: "none" }}
+      >
+        <tbody>
+          <tr style={{ border: "none" }}>
+            <td width="400px" style={{ border: "noe", verticalAlign: "top" }}>
+              <HeaderImage />
+              <RepositoriesImage />
+              <ActivityCommunityImage />
+              <RecentActivityImage />
+            </td>
+            <td width="400px" style={{ border: "noe", verticalAlign: "top" }}>
+              <LanguagesImage />
+              <AchievementsImage />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 }
 
