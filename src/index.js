f1512a39f6dae7741afccf839c2adc98d4973a0d
m-avagyan <martikavagyan1@gmail.com>
2023-01-21 15:46:05 +0400

    wip

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
+import App from "./App.jsx";
+
 const root = document.getElementById("root");
 const app = createRoot(root);
 
-app.render(<h1>test</h1>);
+app.render(<App />);
