7679ed4b32b748b604c6f9759219f0d6f67fb5ed
Martik Avagyan <67459902+m-avagyan@users.noreply.github.com>
2023-01-22 12:28:34 +0400

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-
-import App from "./App.jsx";
-
-const root = document.getElementById("root");
-const app = createRoot(root);
-
-app.render(<App />);
